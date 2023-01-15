4dc2873e54c944bd87b0790c5051db5e845d6a64
Pavel Belov <job.pb@yandex.ru>
2023-01-15 21:11:36 +0300

    add home work task 07

---
+++ HomeWorkTask07/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int intNum = Convert.ToInt32(number);
+
+if (intNum < 100 || intNum > 999)
+{
+    Console.WriteLine("Число должно быть трёхзначным!");
+}
+else
+{
+    Console.Write("Последняя цифра данного числа: ");
+    Console.WriteLine(number[number.Length - 1]);
+}
