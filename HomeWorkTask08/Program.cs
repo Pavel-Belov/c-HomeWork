161f8326a04243aafd514062f8e55d6367efd4d2
Pavel Belov <job.pb@yandex.ru>
2023-01-15 21:15:21 +0300

    add home work task 08

---
+++ HomeWorkTask08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8. Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int intNum = Convert.ToInt32(number);
+
+if (intNum < 100 || intNum > 999)
+{
+    Console.WriteLine("Число должно быть трёхзначным!");
+}
+else
+{
+    Console.Write("Вторая цифра данного числа: ");
+    Console.WriteLine(number[1]);
+}
