10c24c79e438b65d15c29d9af398527842f7225b
Pavel Belov <job.pb@yandex.ru>
2023-01-15 21:43:52 +0300

    add home work task 10

---
+++ HomeWorkTask10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10. Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+
+Console.Write("Случайное трёхзначное число: ");
+Console.WriteLine(number);
+
+int num1 = number / 100;
+int num2 = (number % 100 - number % 10) / 10;
+int num3 = number - num1 * 100 - num2 * 10;
+
+Console.Write("Удаляем вторую цифру числа и получаем: ");
+Console.Write(num1);
+Console.Write(num3);
