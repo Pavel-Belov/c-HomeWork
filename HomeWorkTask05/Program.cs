cc4901eff9d5395146a43eec0199c34c16a06c09
Pavel Belov <job.pb@yandex.ru>
2023-01-15 20:45:07 +0300

    add home work task 05

---
+++ HomeWorkTask05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 5. Выяснить является ли число чётным
+
+int number = new Random().Next(1, 100);
+
+Console.Write("Число ");
+Console.Write(number);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine(" является чётным.");
+}
+else
+{
+    Console.WriteLine(" не является чётным.");
+}
