11f5e4b85e01cb7821735ac02536077e2a1f5a4b
Pavel Belov <job.pb@yandex.ru>
2023-01-15 21:57:46 +0300

    add home work task 12

---
+++ HomeWorkTask12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 12. Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+
+int strLength = num.Length;
+
+if (strLength >= 3)
+{
+    Console.Write("Третья цифра числа: ");
+    Console.WriteLine(num[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры в данном числе нет!");
+}
