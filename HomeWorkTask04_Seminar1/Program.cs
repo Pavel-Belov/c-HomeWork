623c39bb05cad46dbcf39c23ad8455226ffbf3e9
Pavel Belov <job.pb@yandex.ru>
2023-01-15 20:39:13 +0300

    add home work task 04

---
+++ HomeWorkTask04_Seminar1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4. Найти максимальное из трех чисел
+
+int number1 = new Random().Next(1,100);
+int number2 = new Random().Next(1,100);
+int number3 = new Random().Next(1,100);
+
+Console.Write("число 1: ");
+Console.WriteLine(number1);
+Console.Write("число 2: ");
+Console.WriteLine(number2);
+Console.Write("число 3: ");
+Console.WriteLine(number3);
+
+int max = number1;
+
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.Write("Макисмальное число: ");
+Console.WriteLine(max);
