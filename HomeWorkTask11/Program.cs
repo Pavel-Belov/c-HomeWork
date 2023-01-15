b542a5f9eb41876aebe00a89fa6962f0b461236a
Pavel Belov <job.pb@yandex.ru>
2023-01-15 21:51:51 +0300

    add home work task 11

---
+++ HomeWorkTask11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 11. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Задайте число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте кратное число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int mod = num1 % num2;
+
+if (mod == 0)
+{
+    Console.WriteLine("Число " + num2 + " кратно заданному числу " + num1);
+}
+else
+{
+    Console.WriteLine("Число " + num2 + " не кратно заданному числу " + num1);
+    Console.WriteLine("Остаток: " + mod);
+}
