8e0b3e9bee94e2498b9e833a55318d4c945a59ef
Pavel Belov <job.pb@yandex.ru>
2023-01-15 19:20:54 +0300

    add home work task 01

---
+++ HomeWorkTask01_Seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 1. Вывести квадрат числа
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number * number;
+
+Console.Write("Квадрат числа равен: ");
+Console.WriteLine(result);
