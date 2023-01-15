afb1d17d42adea38c19ff240d38cfaad9501ee9d
Pavel Belov <job.pb@yandex.ru>
2023-01-15 20:56:26 +0300

    add home work task 06

---
+++ HomeWorkTask06/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6. Показать чётные числа от 1 до N
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+Console.WriteLine("Чётные числа от 1 до N:");
+
+while (i <= n)
+{
+    Console.Write(i);
+
+    if (i < n - 1)
+    {
+        Console.Write(", ");
+    }
+
+    i += 2;
+}
