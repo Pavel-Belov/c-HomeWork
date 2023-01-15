0dc7a7df93438246b5c8ca3fe9ff18da47947486
Pavel Belov <job.pb@yandex.ru>
2023-01-15 21:25:49 +0300

    add home work task 09

---
+++ HomeWorkTask09/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 9. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 100);
+
+Console.Write("Случайное число из отрезка [10, 100]: ");
+Console.WriteLine(number);
+
+int firstNum = number / 10;
+int secondNum = number % 10;
+
+Console.Write("Наибольшая цифра числа: ");
+if (firstNum > secondNum)
+{
+    Console.WriteLine(firstNum);
+}
+else
+{
+    Console.WriteLine(secondNum);
+}
